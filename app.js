368e1af8e6ec558f314e8b9b2ba30f9dce6780c5
LonwaboMvovo <44nator@gmail.com>
2023-11-23 21:59:19 +0200

    getting dota heroes and info from opendota api and basic display of hero names based on attributes

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("LMAO XD")
+const strHeroes = document.getElementById("str_heroes");
+const agiHeroes = document.getElementById("agi_heroes");
+const intHeroes = document.getElementById("int_heroes");
+const uniHeroes = document.getElementById("uni_heroes");
+
+
+async function getHeroes() {
+    const response = await fetch("https://api.opendota.com/api/heroes");
+    let heroesArray = await response.json();
+
+    console.log(heroesArray);
+
+    for (let hero of heroesArray) {
+        switch (hero.primary_attr) {
+            case "str":
+                strHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
+                break;
+            case "agi":
+                agiHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
+                break;
+            case "int":
+                intHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
+                break;
+            default:
+                uniHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
+        }
+    }
+}
+
+document.addEventListener("load", getHeroes());
