94daf70b849b522ad861e7419a1f4951d8d77451
LonwaboMvovo <44nator@gmail.com>
2023-11-25 22:03:36 +0200

    added filter bar for heroes and now display all heroes in alphabetical order

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-const strHeroes = document.getElementById("str_heroes");
-const agiHeroes = document.getElementById("agi_heroes");
-const intHeroes = document.getElementById("int_heroes");
-const uniHeroes = document.getElementById("uni_heroes");
+const heroes = document.getElementById("heroes");
 
 
 async function getHeroes() {
-    const response = await fetch("https://api.opendota.com/api/heroes");
-    let heroesArray = await response.json();
-
-    console.log(heroesArray);
+    // Don't wanna make API call all the time, so commenting for now and using value from previous call
+    // const response = await fetch("https://api.opendota.com/api/heroes");
+    // let heroesArray = await response.json();
 
     for (let hero of heroesArray) {
-        switch (hero.primary_attr) {
-            case "str":
-                strHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
-                break;
-            case "agi":
-                agiHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
-                break;
-            case "int":
-                intHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
-                break;
-            default:
-                uniHeroes.innerHTML += `<p>${hero.localized_name}</p>`;
-        }
+        heroes.innerHTML += `<p>${hero.localized_name}</p>`;
     }
 }
 
