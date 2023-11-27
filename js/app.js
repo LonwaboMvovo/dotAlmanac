bd7a159297dca7102f8dac9cbfdb332867daec71
LonwaboMvovo <44nator@gmail.com>
2023-11-27 11:38:30 +0200

    can now filte heroes by type

--- js/app.js
+++ js/app.js
@@ -6,29 +6,33 @@ function heroesByAttribute(hero) {
     return heroFilter.attribute.includes(hero.primary_attr);
 }
 
+function heroesByType(hero) {
+    return heroFilter.type.some((type) => hero.roles.includes(type));
+}
+
 function updateHeroesContainer() {
     const heroesContainer = document.getElementById("heroes-container");
     heroesContainer.innerHTML = "";
 
     let heroesToShow = heroesArray
         .filter(heroesBySubstring)
-        .filter(heroesByAttribute);
+        .filter(heroesByAttribute)
+        .filter(heroesByType);
 
     for (let hero of heroesToShow) {
-        heroesContainer.innerHTML += `<p><a href="/${hero.localized_name.replace(/\s/g, '').toLowerCase()}" onclick="route()">${hero.localized_name}</a><p>`;
+        heroesContainer.innerHTML += `<p><a href="/${hero.localized_name.replace(/\s/g, "").toLowerCase()}" onclick="route()">${hero.localized_name}</a><p>`;
     }
 }
 
 async function getHeroes() {
     const response = await fetch("https://api.opendota.com/api/heroes");
     let heroesArray = await response.json();
-
     return heroesArray;
 }
 
 let heroFilter = {
     "attribute": ["str", "agi", "int", "all"],
-    "type": null,
+    "type": ["Carry", "Support"],
     "attack": null,
     "complexity": null,
     "tags": null,
--- js/router.js
+++ js/router.js
@@ -15,20 +15,35 @@ const handleLocation = async () => {
         updateHeroesContainer();
 
         const filterName = document.getElementById("filter-name");
-        const filterAtributes = document.querySelectorAll('.filter-atr');
+        const filterAtribute = document.querySelectorAll(".filter-atr");
+        const filterType = document.querySelectorAll(".filter-type");
 
         filterName.addEventListener("input", () => {
             heroFilter.name = filterName.value;
+
             updateHeroesContainer();
         });
 
-        filterAtributes.forEach((button) => {
-            button.addEventListener('click', () => {
+        filterAtribute.forEach((button) => {
+            button.addEventListener("click", () => {
                 if (heroFilter.attribute.length === 1 && heroFilter.attribute[0] === button.id) {
                     heroFilter.attribute = ["str", "agi", "int", "all"];
                 } else {
                     heroFilter.attribute = [button.id];
                 }
+
+                updateHeroesContainer();
+            });
+        });
+
+        filterType.forEach((button) => {
+            button.addEventListener("click", () => {
+                if (heroFilter.type.length === 1 && heroFilter.type[0] === button.id) {
+                    heroFilter.type = ["Carry", "Support"];
+                } else {
+                    heroFilter.type = [button.id];
+                }
+
                 updateHeroesContainer();
             });
         });
@@ -39,7 +54,7 @@ let routes = {
     "/": "/pages/heroes.html"
 };
 for (let hero of heroesArray) {
-    routes["/" + hero.localized_name.replace(/\s/g, '').toLowerCase()] = "/pages/hero.html"
+    routes["/" + hero.localized_name.replace(/\s/g, "").toLowerCase()] = "/pages/hero.html";
 }
 
 window.onpopstate = handleLocation;
